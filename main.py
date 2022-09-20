fbed3ad9756ac2e6580967a112d18554fdabb6ea
RomanKiado-12 <820418@mystma.org>
2022-09-20 15:18:09 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-#WRITE YOUR CODE IN THIS FILE
+def greaterThan(x, y):
+    if y > x:
+        return "true"
+    else:
+        return "false"
+def lessThan(x, y):
+    if y < x:
+        return "true"
+    else: return "false"
+def equalTo(x, y):
+    if y == x:
+        return "true"
+    else: return "false"
+def greaterOrEqual(x, y):
+    if y >= x:
+        return "true"
+    else: 
+        return "false"
+def lessOrEqual(x, y):
+    if y <= x:
+        return "true"
+    else:
+        return "false"
